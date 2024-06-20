5d18601ec77c92f740038ca231c5d4a95494a515
Ajay Vishnu E <ajayvishnu121@gmail.com>
2024-06-20 10:13:14 +0530

    Initial Commit

---
+++ task_manager.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+import json
+
+class Task:
+    def __init__(self, title, description, due_date, status):
+        self.title = title
+        self.description = description
+        self.due_date = due_date
+        self.status = status
+        
